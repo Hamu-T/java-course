b79d3c986a252234d66b41f335a453aaf40524ac
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-12-09 14:18:32 +0900

    Mapを作成・表示・例外を起こすプログラム

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,48 @@
 package org.example;
+import java.util.HashMap;
+import java.util.Map;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Map<String, String> musicalKeysMap = new HashMap<>();
+        musicalKeysMap.put("C" , "Cメジャースケールは「ハ長調」です。");
+        musicalKeysMap.put("Cm" , "Cマイナースケールは「ハ短調」です。");
+        musicalKeysMap.put("D" , "Dメジャースケールは「二長調」です。");
+        musicalKeysMap.put("Dm" , "Dマイナースケールは「二短調」です。");
+        musicalKeysMap.put("E" , "Eメジャースケールは「ホ長調」です。");
+        musicalKeysMap.put("Em" , "Eマイナースケールは「ホ短調」です。");
+        musicalKeysMap.put("F" , "Fメジャースケールは「ヘ長調」です。");
+        musicalKeysMap.put("Fm" , "Fマイナースケールは「ヘ短調」です。");
+        musicalKeysMap.put("G" , "Gメジャースケールは「ト長調」です。");
+        musicalKeysMap.put("Gm" , "Gマイナースケールは「ト短調」です。");
+        musicalKeysMap.put("A" , "Aメジャースケールは「イ長調」です。");
+        musicalKeysMap.put("Am" , "Aマイナースケールは「イ短調」です。");
+        musicalKeysMap.put("B" , "Bメジャースケールは「ロ長調」です。");
+        musicalKeysMap.put("Bm" , "Bマイナースケールは「ロ短調」です。");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Mapのすべてのエントリを拡張for文で表示
+        for (Map.Entry<String, String> entry : musicalKeysMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        String targetKey = "Q";
+        try {
+            String japaneseDescription = getMusicalKeyDescription(musicalKeysMap, targetKey);
+            System.out.println(japaneseDescription);
+        } catch (myKeyNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+    private static String getMusicalKeyDescription(Map<String, String> japanese, String targetKey) throws myKeyNotFoundException {
+        if (japanese.containsKey(targetKey)) {
+            return japanese.get(targetKey);
+        } else {
+            throw new myKeyNotFoundException("指定された調性が見つかりません : " + targetKey);
+        }
+    }
+    static class myKeyNotFoundException extends Exception {
+        public myKeyNotFoundException(String message) {
+            super(message);
         }
     }
 }
